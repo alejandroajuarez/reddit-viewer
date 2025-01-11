91623c61a751aff77b5f266d1b541bd45d5f9df1
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-11 14:40:59 -0700

    TTY Prompt/progress bar added

--- reddit.rb
+++ reddit.rb
@@ -1,14 +1,28 @@
 require "http"
 require "tty-prompt"
 require "json"
+require "tty-progressbar"
 
 prompt = TTY::Prompt.new
+bar = TTY::ProgressBar.new("Grabbing subreddits from the Etheral Plane [:bar]", bar_format: :box, total:30)
 
+# Greeting Message
 puts "Welcome to the Reddit Terminal App!"
 puts "Enter the Subreddit you want to view: "
-user_input = gets.chomp
+user_input = gets.chomp.downcase
 
-response = HTTP.get("https://www.reddit.com/.json#{user_input}")
+# Retrival of subreddit message
+puts "Please wait a moment while I retrieve posts from /r/#{user_input}"
+
+30.times do
+  sleep(0.05)
+  bar.advance
+end
+
+response = HTTP.headers(
+  "User-Agent" => "Mac Terminal App/1.0"
+).get("https://www.reddit.com/r/#{user_input}/top.json?limit=10")
+
 data = response.parse
 
-# pp reddit_data
+puts data
