b93a99e3fa4e43e4f89e730bf0c96b42e25e8c6f
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-12 00:53:31 -0700

    Fixed reddit post author names and added the ability for the user to pick how many comments to see from the selected post

--- reddit.rb
+++ reddit.rb
@@ -65,12 +65,12 @@ begin
 
     posts.each_with_index do |post, index|
       post_data = post["data"]
-      prompt.say("#{index + 1}. #{post_data['title']}", color: :bright_blue) # Post's title
-      prompt.say("    Posted by: u/#{post_data['author_fullname']}") # Post's Author
-      prompt.say("    #{post_data['ups']} upvotes") # Post's Upvotes
-      prompt.say("    #{post_data['num_comments']} comments.") # Post's comments
-      prompt.say("    #{post_data['url']}") # Post's URL
-      prompt.say("    " + "=" * 147)# lines for clarity
+      prompt.say("#{index + 1}. #{post_data['title']}\n", color: :bright_blue) # Post's title
+      prompt.say("Posted by: u/#{post_data['author']}") # Post's Author
+      prompt.say("#{post_data['ups']} upvotes") # Post's Upvotes
+      prompt.say("#{post_data['num_comments']} comments.") # Post's comments
+      prompt.say("#{post_data['url']}") # Post's URL
+      prompt.say("" + "=" * 147)# lines for clarity
     end
 
     # Let user select a post using TTY-prompt
@@ -78,12 +78,48 @@ begin
     selected_post = prompt.select("Select a post to see more details using your up and down arrow keys:", 
     posts.map { |post| post['data']['title'] })
 
+    # This validates the selection the user made with the info from the .json array
     matching_post = posts.find { |post| post["data"]["title"] == selected_post }
     if matching_post
       selected_post_data = matching_post["data"]
       prompt.say("\nDetailed view:", color: :bright_yellow)
       prompt.say(selected_post_data["selftext"]) if selected_post_data["selftext"].to_s.length > 0
-      prompt.say("Reddit URL: https://reddit.com#{selected_post_data['permalink']}")
+      
+      # Fetch comments for the selected post
+      comments_response = HTTP.headers(
+        "User-Agent" => "Mac Terminal App/1.0"
+      ).get("https://www.reddit.com#{selected_post_data['permalink']}.json")
+      
+      comments_data = comments_response.parse
+      
+      # Comments are in the second element of the array
+      if comments_data[1] && comments_data[1]["data"]["children"]
+        available_comments = comments_data[1]["data"]["children"].reject { |c| c["data"]["stickied"] }
+        max_comments = available_comments.length
+      
+        if max_comments > 0
+          comment_count = prompt.select("How many comments would you like to see?") do |menu|
+            menu.choice "5 comments", 5
+            menu.choice "10 comments", 10
+            menu.choice "20 comments", 20
+            menu.choice "All comments", max_comments
+          end
+          
+          comments = available_comments.take(comment_count)
+          
+          prompt.say("\nTop #{comment_count} Comments:", color: :bright_yellow)
+          comments.each do |comment|
+            prompt.say("\nu/#{comment['data']['author']}", color: :bright_blue)
+            prompt.say("#{comment['data']['body']}")
+            prompt.say("#{comment['data']['ups']} upvotes")
+            prompt.say("-" * 50)
+          end
+        else
+          prompt.say("\nNo comments found for this post.", color: :bright_yellow)
+        end
+      end
+
+      prompt.say("\nReddit URL: https://reddit.com#{selected_post_data['permalink']}")
     else
       prompt.error("Couldn't find the selected post data")
     end
